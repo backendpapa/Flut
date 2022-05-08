3417f3dc819b8044c81379ceaeee40e6c20358cb
Backendpapa <98647710+backendpapa@users.noreply.github.com>
2022-05-08 01:45:35 +0100

    added event header

--- screens/SingleEvent/SingleEvent.js
+++ screens/SingleEvent/SingleEvent.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {View, Text,StatusBar} from 'react-native';
+import SEHeader from "./components/SEHeader/SEHeader";
 
 function SingleEvent() {
   return (
     <View>
       <StatusBar translucent backgroundColor='transparent' />
-      <Text>hello single event</Text>
+      <SEHeader />
+
     </View>
   );
 }
---
+++ screens/SingleEvent/components/SEHeader/SEHeader.js
@@ -0,0 +1,82 @@
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  ImageBackground,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
+import event_img from '../../../../assets/images/event_img.png';
+import {Icon} from '@rneui/base';
+
+function SEHeader() {
+  const seh = se_header;
+  const [like, setLike] = useState(false);
+  return (
+    <View>
+      <ImageBackground style={seh.container} source={event_img}>
+        <View style={seh.inner_container}>
+          <View>
+            {/*  Back to previous screen icon*/}
+            <TouchableOpacity activeOpacity={0.8}>
+              <Icon
+                name={'arrow-left'}
+                color={'white'}
+                type={'material-community'}
+              />
+            </TouchableOpacity>
+          </View>
+          <View style={seh.icon_right}>
+            <TouchableOpacity
+              onPress={() => {
+                setLike(!like);
+              }}
+              activeOpacity={0.8}>
+              <Icon
+                name={like == true ? 'cards-heart' : 'cards-heart-outline'}
+                color={'white'}
+                type={'material-community'}
+              />
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              style={{marginLeft: 10}}
+              onPress={() => {
+                setLike(!like);
+              }}
+              activeOpacity={0.8}>
+              <Icon
+                name={'ios-share-social-outline'}
+                color={'white'}
+                type={'ionicon'}
+              />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </ImageBackground>
+      <Text>hEADERfff</Text>
+    </View>
+  );
+}
+export default SEHeader;
+
+const se_header = StyleSheet.create({
+  container: {
+    height: 250,
+  },
+  inner_container: {
+    position: 'absolute',
+    bottom: 0,
+    width: '100%',
+    padding: 15,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  icon_right: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+});
